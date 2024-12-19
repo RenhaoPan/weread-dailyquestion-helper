76b31416df485d2a4e0a0d3862e853679c26b591
Giant <770391423@qq.com>
2024-12-19 19:20:00 +0800

    初步优化

--- process/OCR.py
+++ process/OCR.py
@@ -14,12 +14,19 @@ class OCR:
 
     def _ocr(self, img):
         imgBin = self._pil2bin(img)
-        return self.client.basicGeneral(imgBin)
+        #return self.client.webImage(imgBin)  # 网络图片文字识别 1000次/月
+        return self.client.basicGeneral(imgBin)           #通用文字识别（标准版）1000次/月
+        #return self.client.general(imgBin)                  #通用文字识别（标准含位置）1000次/月
+        #return self.client.basicAccurate(imgBin)        #通用文字识别（高精度版）1000次/月
+        #return self.client.accurate(imgBin)             #通用文字识别（高精度含位置版）500次/月
+
+
 
     def run(self, quesImg, answImg):
         ques = self._ocr(quesImg)
         answ = self._ocr(answImg)
         ques = ''.join([item['words'] for item in ques['words_result']])
         answ = [item['words'] for item in answ['words_result']]
+
         return ques, answ
 
--- process/ScreenCapture.py
+++ process/ScreenCapture.py
@@ -1,46 +1,66 @@
 # -*- coding: utf-8 -*-
 import win32gui, win32con
+import ctypes
+from ctypes import wintypes
 from PIL import ImageGrab
+
 
 class ScreenCapture:
     def __init__(self):
-        self.hwnd = win32gui.FindWindow(0, "微信读书")
+        self.hwnd = win32gui.FindWindow(None, "微信读书")
         if self.hwnd == 0:
             raise Exception("没有找到'微信读书'小程序")
-        self.bound = win32gui.GetWindowRect(self.hwnd)
+        # self.bound = win32gui.GetWindowRect(self.hwnd)
+        self.bound = self._get_window_rect(self.hwnd)
         self.rpx = self._rpx2px(self.bound[2] - self.bound[0])
 
     # rpx转px
     def _rpx2px(self, base):
         ratio = base / 750
+
         def _rpx(rpx):
             return rpx * ratio
+
         return _rpx
+
+    # 获取真实的窗口 POS
+    def _get_window_rect(self, hwnd):
+        try:
+            f = ctypes.windll.dwmapi.DwmGetWindowAttribute
+        except WindowsError:
+            f = None
+        if f:
+            rect = ctypes.wintypes.RECT()
+            f(ctypes.wintypes.HWND(hwnd), ctypes.wintypes.DWORD(9), ctypes.byref(rect), ctypes.sizeof(rect))
+            return rect.left, rect.top, rect.right, rect.bottom
 
     # 截图
     def _getCapture(self):
         img = ImageGrab.grab(self.bound)
+        # img.show()
+        # img.save("./img/3.png")
         return img
-    
+
     # 切割
     def _splitCapture(self, img):
         quesImg = img.crop((self.rpx(85), self.rpx(460), self.rpx(670), self.rpx(590)))
+        # quesImg.save("./img/1.png")
         ansImg = img.crop((self.rpx(85), self.rpx(590), self.rpx(670), self.rpx(1055)))
+        # ansImg.save("./img/2.png")
         return quesImg, ansImg
-    
-    
+
     def run(self):
         img = self._getCapture()
         return self._splitCapture(img)
-        
 
 
-        
 
-        
-        
 
-        
+
+
+
+
+
 
 
 
